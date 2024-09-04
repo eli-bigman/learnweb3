859272db561250a1b20ee44d121f5294b9e756c4
eli-bigman <nutsugah81@gmail.com>
2024-09-04 16:15:32 +0000

    created chainlink pluging use case

---
+++ src/app/chainlink/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Chainlink from '@/components/component/chainlink';
+
+const ChainlinkPage = () => {
+  return (
+    <div>
+      <Chainlink />
+    </div>
+  );
+};
+
+export default ChainlinkPage;
