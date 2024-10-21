a4c412181e4f65db74f5975585c31bdd966dee09
elibigman <nutsugah81@gmail.com>
2024-10-21 00:59:36 +0000

    update the ui with prism

--- src/components/component/namespace.jsx
+++ src/components/component/namespace.jsx
@@ -4,7 +4,7 @@ import CodeDisplay from "@/components/component/CodeDisplay";
 import AIResponse from "@/components/component/AIResponse";
 
 export default function Namespace() {
-  const codeResponse = ` 
+  const code = ` 
     // import web3 and plugin
 import { Web3 } from "web3";
 import { Chain, EnsPlugin } from "@namespace-ens/web3-plugin-ens";
@@ -32,15 +32,17 @@ main();
 
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground">
-      <main className="container mx-auto grid grid-cols-1 gap-8 p-4 sm:grid-cols-2 lg:grid-cols-3 lg:p-8">
-        <div className="col-span-1 lg:col-span-2 pl-5">
-          <Playground />
-        </div>
-        <div className="col-span-1 lg:col-span-1 flex flex-col gap-8">
-          <CodeDisplay codeResponse={codeResponse} />
-          <AIResponse promptPrefix={prompt}/>
-        </div>
-      </main>
-    </div>
+    <main className="container mx-auto grid grid-cols-1 gap-8 p-4 sm:grid-cols-2 lg:grid-cols-3 lg:p-8">
+      <div className="col-span-1 lg:col-span-2">
+        <Playground />
+      </div>
+      <div className="col-span-1 lg:col-span-2">
+        <CodeDisplay code={code} />
+      </div>
+      <div className="col-span-1 lg:col-span-1">
+        <AIResponse promptPrefix={prompt}/>
+      </div>
+    </main>
+  </div>
   );
 }
