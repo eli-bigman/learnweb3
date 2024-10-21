f0e2eb0647e6b424deb5fd69ba5692280a3191a8
elibigman <nutsugah81@gmail.com>
2024-10-21 00:59:10 +0000

    update the ui with prism

--- src/components/component/chainlink.jsx
+++ src/components/component/chainlink.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import Header from "@/components/component/Header";
 import Playground from "@/components/component/chainlinkPlayground";
@@ -5,7 +6,7 @@ import CodeDisplay from "@/components/component/CodeDisplay";
 import AIResponse from "@/components/component/AIResponse";
 
 export default function Chainlink() {
-  const codeResponse = ` 
+  const code = ` 
     // Array of cryptocurrency coins with their names and Chainlink price feed symbols
 const coins = [
   { name: "Bitcoin", symbol: "BtcUsd" },
@@ -99,16 +100,18 @@ export default function ChainlinkGame() {
 
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground">
-      <Header />
-      <main className="container mx-auto grid grid-cols-1 gap-8 p-4 sm:grid-cols-2 lg:grid-cols-3 lg:p-8">
-        <div className="col-span-1 lg:col-span-2 pl-5">
-          <Playground />
-        </div>
-        <div className="col-span-1 lg:col-span-1 flex flex-col gap-8">
-          <CodeDisplay codeResponse={codeResponse} />
-          <AIResponse promptPrefix={prompt}/>
-        </div>
-      </main>
-    </div>
+    <Header />
+    <main className="container mx-auto grid grid-cols-1 gap-8 p-4 sm:grid-cols-2 lg:grid-cols-3 lg:p-8">
+      <div className="col-span-1 lg:col-span-2">
+        <Playground />
+      </div>
+      <div className="col-span-1 lg:col-span-2">
+        <CodeDisplay code={code} />
+      </div>
+      <div className="col-span-1 lg:col-span-1">
+        <AIResponse promptPrefix={prompt}/>
+      </div>
+    </main>
+  </div>
   );
 }
